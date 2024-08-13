a46944bfc5268c4a096c908024591ac16df0cea4
Gerardo Padilla <laguilar@softnet.mx>
2024-08-13 01:04:36 -0600

    Initial commit

---
+++ SmtpServerWinForms-main/SmtpServerWinForms/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace SmtpServerWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.btnSendEmail = new System.Windows.Forms.Button();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.lblBody = new System.Windows.Forms.Label();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.lblLog = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.Location = new System.Drawing.Point(862, 12);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(75, 23);
+            this.btnStartServer.TabIndex = 0;
+            this.btnStartServer.Text = "SMTP";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            this.btnStartServer.Click += new System.EventHandler(this.btnStartServer_Click);
+            // 
+            // btnSendEmail
+            // 
+            this.btnSendEmail.Location = new System.Drawing.Point(472, 415);
+            this.btnSendEmail.Name = "btnSendEmail";
+            this.btnSendEmail.Size = new System.Drawing.Size(75, 23);
+            this.btnSendEmail.TabIndex = 1;
+            this.btnSendEmail.Text = "Send";
+            this.btnSendEmail.UseVisualStyleBackColor = true;
+            this.btnSendEmail.Click += new System.EventHandler(this.btnSendEmail_Click);
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(41, 32);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(76, 17);
+            this.lblFrom.TabIndex = 2;
+            this.lblFrom.Text = "From (De):";
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(41, 62);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(73, 17);
+            this.lblTo.TabIndex = 3;
+            this.lblTo.Text = "To (Para):";
+            // 
+            // lblSubject
+            // 
+            this.lblSubject.AutoSize = true;
+            this.lblSubject.Location = new System.Drawing.Point(41, 100);
+            this.lblSubject.Name = "lblSubject";
+            this.lblSubject.Size = new System.Drawing.Size(117, 17);
+            this.lblSubject.TabIndex = 4;
+            this.lblSubject.Text = "Subject (Asunto):";
+            // 
+            // lblBody
+            // 
+            this.lblBody.AutoSize = true;
+            this.lblBody.Location = new System.Drawing.Point(41, 138);
+            this.lblBody.Name = "lblBody";
+            this.lblBody.Size = new System.Drawing.Size(109, 17);
+            this.lblBody.TabIndex = 5;
+            this.lblBody.Text = "Message (msg):";
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(176, 29);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(371, 22);
+            this.txtFrom.TabIndex = 6;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(176, 62);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(371, 22);
+            this.txtTo.TabIndex = 7;
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(176, 97);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(371, 22);
+            this.txtSubject.TabIndex = 8;
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(44, 168);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(503, 234);
+            this.txtBody.TabIndex = 9;
+            // 
+            // lblLog
+            // 
+            this.lblLog.AutoSize = true;
+            this.lblLog.Location = new System.Drawing.Point(567, 34);
+            this.lblLog.Name = "lblLog";
+            this.lblLog.Size = new System.Drawing.Size(36, 17);
+            this.lblLog.TabIndex = 10;
+            this.lblLog.Text = "Log:";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(570, 62);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(367, 340);
+            this.txtLog.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(949, 450);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.lblLog);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.lblBody);
+            this.Controls.Add(this.lblSubject);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.btnSendEmail);
+            this.Controls.Add(this.btnStartServer);
+            this.Name = "Form1";
+            this.Text = "Send Mail Dummy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartServer;
+        private System.Windows.Forms.Button btnSendEmail;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.Label lblSubject;
+        private System.Windows.Forms.Label lblBody;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.Label lblLog;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ SmtpServerWinForms-main/SmtpServerWinForms/Form1.cs
@@ -0,0 +1,81 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using SmtpServer;
+using SmtpServer.Storage;
+using System;
+using System.Buffers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SmtpServerWinForms
+{
+    public partial class Form1 : Form
+    {
+        private SmtpServer.SmtpServer _smtpServer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public class SampleMessageStore : MessageStore
+        {
+            public override Task<SmtpServer.Protocol.SmtpResponse> SaveAsync(ISessionContext context, IMessageTransaction transaction, ReadOnlySequence<byte> buffer, CancellationToken cancellationToken)
+            {
+                var message = Encoding.UTF8.GetString(buffer.ToArray());
+
+                MessageBox.Show("Received message:\n" + message);
+
+                return Task.FromResult(SmtpServer.Protocol.SmtpResponse.Ok);
+            }
+        }
+
+        private void btnStartServer_Click(object sender, EventArgs e)
+        {
+            var options = new SmtpServerOptionsBuilder()
+            .ServerName("localhost")
+            .Port(25, 587) // SMTP ports
+            .Build();
+
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IMessageStore, SampleMessageStore>()
+                .BuildServiceProvider();
+
+            _smtpServer = new SmtpServer.SmtpServer(options, serviceProvider);
+
+            var serverTask = _smtpServer.StartAsync(CancellationToken.None);
+
+            MessageBox.Show("SMTP server is running...");
+        }
+
+        private void btnSendEmail_Click(object sender, EventArgs e)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Sender Name", txtFrom.Text));
+            message.To.Add(new MailboxAddress("Receiver Name", txtTo.Text));
+            message.Subject = txtSubject.Text;
+
+            message.Body = new TextPart("plain")
+            {
+                Text = txtBody.Text
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("localhost", 25, false);
+
+                // Si el servidor requiere autenticación, descomente y configure lo siguiente:
+                // client.Authenticate("username", "password");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+
+            MessageBox.Show("Email sent successfully!");
+        }
+    }
+}
